c9ba82b055fdd2e6baea29c25790eda3a2b91985
dpv <dpopovvelasco@gmail.com>
2024-07-28 19:22:28 -0400

    add first airflow dag script for getting and pushing bird obs

---
+++ get_and_push_dag.py
@@ -0,0 +1,32 @@
+from datetime import datetime, timedelta
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+
+default_args = {
+    'owner': 'airflow',
+    'start_date': datetime(2024, 7, 28),
+    'depends_on_past': False,
+    'email':['dpopovvelasco@gmail.com'],
+    'email_on_failure': True,
+    'email_on_retry': True,
+    'email_on_success': True,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5)
+    }
+
+dag = DAG(
+    default_args=default_args,
+    description='A simple DAG to get and push bird observations',
+    schedule_interval=timedelta(days=3),
+    catchup=False #disable backfilling
+)
+
+with dag:
+    get_bird_observations = BashOperator(
+        task_id='get_and_push_bird_observations',
+        bash_command='python3 get_data_and_push.py',
+    )
+
+ 
+
+
